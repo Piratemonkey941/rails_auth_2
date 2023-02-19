0603e87663fad408d3849d456820d89ae14a35b3
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-02-19 16:36:22 -0600

    bootstrap added, navbar implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  # get 'pages/home'
+  # get 'pages/about_us'
+
+  
   resources :categories
-  root to: "blogs#index"
-  # get '/about-us', to: "pages#about_us"
+  # root to: "blogs#index"
+  root to: "pages#home"
+  get '/about-us', to: "pages#about_us"
 
   resources :blogs
 
